46cf87764346918669a82ca3aee8410e9325274f
christinelacanilao <57382843+christinelacanilao@users.noreply.github.com>
2020-04-08 08:06:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Footer, new copyright year
 // Sourced from http://updateyourfooter.com/
+// Referenced from https://www.digitalocean.com/community/tutorials/how-to-add-javascript-to-html
 var d = new Date()
 document.write(d.getFullYear())
